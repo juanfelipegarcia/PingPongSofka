eefe7f0e9b6d1289d9b64581b3a7994c9eb95d08
juanfelipegarcia <jfgarcia064@misena.edu.co>
2021-11-17 17:19:42 -0500

    Se generan las barras para  el juego

--- JS/main.js
+++ JS/main.js
@@ -9,10 +9,31 @@
   }
   
   self.Board.prototype={
-    get element() {
+    get elements() {
       var elements = this.bars;
-      elements.push(ball);
+      elements.push(this.ball);
       return elements;
+    }
+  }
+})();
+
+(function(){
+  self.Bar = function(x,y, width, height, board){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.board = board;
+    this.board.bars.push(this);
+    this.kind = "rectangle";
+  }
+
+  self.Bar.prototype = {
+    downd: function(){
+
+    },
+    update: function(){
+
     }
   }
 })();
@@ -25,6 +46,28 @@
     this.board = board;
     this.ctx = canvas.getContext("2d");
   }
+
+  self.BoardView.prototype = {
+    draw: function(){
+      for (var i = this.board.elements.length - 1; i >= 0; i--){
+        var el = this.board.elements[i];
+
+        draw(this.ctx, el);
+      };
+    }
+  }
+
+  function draw(ctx, element){
+    if (element !== null && element.hasOwnProperty("kind")) {
+      switch (element.kind) {
+        case "rectangle":
+          ctx.fillRect(element.x,element.y,element.width,element.height);
+          break;
+      }
+    }
+    
+  }
+
 })();
 
 addEventListener("load",main);
@@ -32,7 +75,11 @@ window.addEventListener("load",main);
 
 function main(){
   var board = new Board(800,400);
+  var bar = new Bar(20,100,40,100,board);
+  var bar = new Bar(730,100,40,100,board);
   var canvas = document.getElementById("canvas");
   var board_view = new BoardView(canvas,board);
+  console.log(board);
+  board_view.draw();
 
 }
