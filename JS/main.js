387601517a5d1d97fe5c1eb7c16ad07e89025445
juanfelipegarcia <jfgarcia064@misena.edu.co>
2021-11-17 09:01:29 -0500

    Se genera el tablero donde se realizara el juego

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,38 @@
-function main(){
+(function(){
+  self.Board = function(width, height){
+    this.width = width;
+    this.height = height;
+    this.playing = false;
+    this.game_over = false;
+    this.bars = [];
+    this.ball = null;
+  }
+  
+  self.Board.prototype={
+    get element() {
+      var elements = this.bars;
+      elements.push(ball);
+      return elements;
+    }
+  }
+})();
 
+(function(){
+  self.BoardView = function(canvas, board){
+    this.canvas = canvas;
+    this.canvas.width = board.width;
+    this.canvas.height = board.height;
+    this.board = board;
+    this.ctx = canvas.getContext("2d");
+  }
+})();
+
+addEventListener("load",main);
+window.addEventListener("load",main);
+
+function main(){
+  var board = new Board(800,400);
+  var canvas = document.getElementById("canvas");
+  var board_view = new BoardView(canvas,board);
 
 }
